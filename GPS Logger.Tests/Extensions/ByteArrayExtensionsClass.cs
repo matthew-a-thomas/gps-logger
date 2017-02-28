4f35d0b25d85da01378ceab3fe11b80df6465741
Matthew Thomas <me@matthewathomas.com>
2017-02-27 20:02:10 -0500

    Started ByteArrayExtensions unit tests

---
+++ GPS Logger.Tests/Extensions/ByteArrayExtensionsClass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GPS_Logger.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GPS_Logger.Tests.Extensions
+{
+    [TestClass]
+    public class ByteArrayExtensionsClass
+    {
+        [TestClass]
+        public class FromHexStringMethod
+        {
+            [TestMethod]
+            public void ReturnsNullWhenGivenNumm()
+            {
+                var b = ByteArrayExtensions.FromHexString(null);
+                Assert.IsNull(b);
+            }
+        }
+    }
+}
