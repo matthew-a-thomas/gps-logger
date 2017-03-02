9c78b657e95e9ea49f5b3045b6011f517b2d672c
Matthew Thomas <me@matthewathomas.com>
2017-03-01 23:06:52 -0500

    Added ReplayDetector, and stubbed out test class

---
+++ GPS Logger.Tests/Utilities/ReplayDetectorClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GPS_Logger.Tests.Utilities
+{
+    [TestClass]
+    public class ReplayDetectorClass
+    {
+        [TestClass]
+        public class IsNewMethod
+        {
+            
+        }
+    }
+}
