fe22759924e0481f314990f2891176a414e4d759
Matthew Thomas <me@matthewathomas.com>
2017-03-11 15:22:45 -0500

    Added an ephemeron, which simple invokes a callback when it is disposed
    This can be used in conjunction with a ConditionalWeakTable to be
    notified when another object is finalized

---
+++ Common.Tests/Utilities/EphemeronClass.cs
@@ -0,0 +1,33 @@
+﻿using Common.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Tests.Utilities
+{
+    [TestClass]
+    public class EphemeronClass
+    {
+        [TestMethod]
+        public void InvokesCallbackOnFinalization()
+        {
+            var invoked = false;
+            Task.Run(() => new Ephemeron(() => invoked = true)).Wait();
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Assert.IsTrue(invoked);
+        }
+
+        [TestMethod]
+        public void DoesNotInvokeCallbackWhenNotFinalized()
+        {
+            var invoked = false;
+            var ephemeron = new Ephemeron(() => invoked = true);
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Assert.IsFalse(invoked);
+        }
+    }
+}
---
+++ Common/Utilities/Ephemeron.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Common.Utilities
+{
+    public class Ephemeron
+    {
+        private readonly Action _callback;
+
+        public Ephemeron(Action callback)
+        {
+            _callback = callback;
+        }
+
+        ~Ephemeron()
+        {
+            _callback();
+        }
+    }
+}
