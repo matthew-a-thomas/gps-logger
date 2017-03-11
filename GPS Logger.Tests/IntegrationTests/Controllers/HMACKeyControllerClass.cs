9361c41b24aa3ea42771dc0001798555895596dc
Matthew Thomas <me@matthewathomas.com>
2017-03-11 15:10:42 -0500

    Added tests for HMAC controller

---
+++ GPS Logger.Tests/IntegrationTests/Controllers/HMACKeyControllerClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GPS_Logger.Tests.IntegrationTests.Controllers
+{
+    [TestClass]
+    public class HMACKeyControllerClass
+    {
+        private const string Root = "/api/hmackey";
+        private readonly TestServer _server = Helpers.CreateServer();
+
+        [TestMethod]
+        public void ReturnsBoolean()
+        {
+            var response = Helpers.Get(_server, Root);
+            bool.Parse(response);
+        }
+    }
+}
