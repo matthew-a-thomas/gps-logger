a783d8a264c2ea5b403d3b097f89a2e06f62220f
Matthew Thomas <me@matthewathomas.com>
2017-03-11 22:36:05 -0500

    Removed .Equals and .GetHashCode
    Since it shouldn't be up to the message to determine uniqueness

--- Common/Messages/Message.cs
+++ Common/Messages/Message.cs
@@ -11,18 +11,5 @@ namespace Common.Messages
         public string ID { get; set; }
         public string Salt { get; set; }
         public long UnixTime { get; set; }
-
-        protected virtual IEnumerable<object> GetFields() => new object[] { Contents, ID, Salt, UnixTime };
-        public override int GetHashCode() => GetFields().Where(x => !ReferenceEquals(x, null)).Aggregate(0, (accumulate, x) => accumulate + x.GetHashCode());
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(obj, null) || !(obj.GetType().Equals(GetType())))
-                return false;
-            var ourFields = GetFields();
-            var theirFields = (obj as Message<T>)?.GetFields();
-            var together = ourFields.Zip(theirFields, Tuple.Create);
-            var allEqual = together.All(tuple => tuple.Item1?.Equals(tuple.Item2) ?? false);
-            return allEqual;
-        }
     }
 }
--- Common/Security/Signing/SignedMessage.cs
+++ Common/Security/Signing/SignedMessage.cs
@@ -7,7 +7,5 @@ namespace Common.Security.Signing
     public class SignedMessage<T> : Message<T>, ISignable
     {
         public string HMAC { get; set; }
-
-        protected override IEnumerable<object> GetFields() => base.GetFields().Concat(new[] { HMAC });
     }
 }
