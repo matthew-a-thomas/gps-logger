1bb1e8dd7ca23df8aabda75d24fe1814b05cdc03
Matthew Thomas <me@matthewathomas.com>
2017-03-11 21:52:48 -0500

    Added unit tests for SignedMessage class

---
+++ Common.Tests/Security/Signing/SignedMessageClass.cs
@@ -0,0 +1,47 @@
+﻿using Common.Security.Signing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common.Tests.Security.Signing
+{
+    [TestClass]
+    public class SignedMessageClass
+    {
+        private static IReadOnlyList<SignedMessage<bool>> CreateDuplicates(int count) => Enumerable.Repeat(0, count).Select(x => new SignedMessage<bool> { Contents = false, HMAC = "hmac", ID = "id", Salt = "salt", UnixTime = DateTimeOffset.Now.ToUnixTimeSeconds() }).ToList();
+
+        [TestClass]
+        public class GetHashCodeMethod
+        {
+            [TestMethod]
+            public void TwoInstancesWithSameValuesHaveSameHashCode()
+            {
+                var instances = CreateDuplicates(2);
+                var hashCode = instances.FirstOrDefault()?.GetHashCode();
+                foreach (var otherInstance in instances.Skip(1))
+                {
+                    Assert.AreEqual(hashCode, otherInstance.GetHashCode());
+                }
+            }
+        }
+
+        [TestClass]
+        public class EqualsMethod
+        {
+            [TestMethod]
+            public void TwoInstancesWithSameValuesAreEqual()
+            {
+                var instances = CreateDuplicates(2);
+                foreach (var instance in instances)
+                {
+                    foreach (var otherInstance in instances.Except(new[] { instance }))
+                    {
+                        Assert.IsTrue(instance.Equals(otherInstance));
+                    }
+                }
+            }
+        }
+    }
+}
