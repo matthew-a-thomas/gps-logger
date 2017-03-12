1450f5a684494df163bcf8b8c91c5cc8e7bfa235
Matthew Thomas <me@matthewathomas.com>
2017-03-11 22:29:25 -0500

    Fixed SignedMessage unit tests

--- Common/Messages/Message.cs
+++ Common/Messages/Message.cs
@@ -1,4 +1,8 @@
-﻿namespace Common.Messages
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.Messages
 {
     public class Message<T>
     {
@@ -7,5 +11,18 @@
         public string ID { get; set; }
         public string Salt { get; set; }
         public long UnixTime { get; set; }
+
+        protected virtual IEnumerable<object> GetFields() => new object[] { Contents, ID, Salt, UnixTime };
+        public override int GetHashCode() => GetFields().Where(x => !ReferenceEquals(x, null)).Aggregate(0, (accumulate, x) => accumulate + x.GetHashCode());
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(obj, null) || !(obj.GetType().Equals(GetType())))
+                return false;
+            var ourFields = GetFields();
+            var theirFields = (obj as Message<T>)?.GetFields();
+            var together = ourFields.Zip(theirFields, Tuple.Create);
+            var allEqual = together.All(tuple => tuple.Item1?.Equals(tuple.Item2) ?? false);
+            return allEqual;
+        }
     }
 }
--- Common/Security/Signing/SignedMessage.cs
+++ Common/Security/Signing/SignedMessage.cs
@@ -1,9 +1,13 @@
-﻿using Common.Messages;
+﻿using System.Collections.Generic;
+using Common.Messages;
+using System.Linq;
 
 namespace Common.Security.Signing
 {
     public class SignedMessage<T> : Message<T>, ISignable
     {
         public string HMAC { get; set; }
+
+        protected override IEnumerable<object> GetFields() => base.GetFields().Concat(new[] { HMAC });
     }
 }
