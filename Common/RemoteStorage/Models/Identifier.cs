c976970075a8dc638f6e07b06bced1ea3415bc16
Matthew Thomas <me@matthewathomas.com>
2017-03-17 18:10:48 -0400

    Adding preliminary database models

---
+++ Common/RemoteStorage/Models/Identifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.RemoteStorage.Models
+{
+    public class Identifier
+    {
+        public int ID { get; set; }
+
+        public byte[] Hex { get; set; }
+    }
+}
