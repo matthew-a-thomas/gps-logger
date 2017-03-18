ca039589fa370e1d67f05680c7ac208eee7b62ef
Matthew Thomas <me@matthewathomas.com>
2017-03-17 20:00:01 -0400

    Added command/query for locations

---
+++ Common/RemoteStorage/Command/ILocationPoster.cs
@@ -0,0 +1,12 @@
+﻿using Common.RemoteStorage.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.RemoteStorage.Command
+{
+    public interface ILocationPoster
+    {
+        void PostLocation(byte[] identifier, Location location);
+    }
+}
---
+++ Common/RemoteStorage/Query/ILocationProvider.cs
@@ -0,0 +1,12 @@
+﻿using Common.RemoteStorage.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common.RemoteStorage.Query
+{
+    public interface ILocationProvider
+    {
+        IEnumerable<IdentifiedLocation> GetAllLocations(byte[] forIdentifier);
+    }
+}
