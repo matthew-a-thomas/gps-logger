3fb79b9a6fd3cb61a0c512a980cfe52fc6bfc28c
Matthew Thomas <me@matthewathomas.com>
2017-03-18 19:06:51 -0400

    Added another test

--- SQLDatabase.Tests/RemoteStorage/Command/IdentifierPosterClass.cs
+++ SQLDatabase.Tests/RemoteStorage/Command/IdentifierPosterClass.cs
@@ -23,6 +23,19 @@ namespace SQLDatabase.Tests.RemoteStorage.Command
                     var id = await poster.PostOrGetIdentifierAsync(transaction, identifier);
                 });
             }
+
+            [TestMethod]
+            public async Task ReturnsTheIDThatWasJustCreated()
+            {
+                await TransactionClass.DoWithTransactionAsync(async transaction =>
+                {
+                    var poster = new IdentifierPoster();
+                    var identifier = Guid.NewGuid().ToByteArray();
+                    var id = await poster.PostOrGetIdentifierAsync(transaction, identifier);
+                    var secondID = await poster.PostOrGetIdentifierAsync(transaction, identifier);
+                    Assert.AreEqual(id, secondID);
+                });
+            }
         }
     }
 }
