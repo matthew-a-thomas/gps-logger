a2cbdea4bbe9912b6d817a391f520d5cd9d6f830
Matthew Thomas <me@matthewathomas.com>
2017-03-18 19:05:11 -0400

    Fleshed out tests

--- SQLDatabase.Tests/RemoteStorage/Command/IdentifierPosterClass.cs
+++ SQLDatabase.Tests/RemoteStorage/Command/IdentifierPosterClass.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SQLDatabase.RemoteStorage.Command;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SQLDatabase.Tests.RemoteStorage.Command
 {
-    class IdentifierPosterClass
+    [TestClass]
+    public class IdentifierPosterClass
     {
+        [TestClass]
+        public class PostOrGetIdentifierAsyncMethod
+        {
+            [TestMethod]
+            public async Task ReturnsSomething()
+            {
+                await TransactionClass.DoWithTransactionAsync(async transaction =>
+                {
+                    var poster = new IdentifierPoster();
+                    var identifier = Guid.NewGuid().ToByteArray();
+                    var id = await poster.PostOrGetIdentifierAsync(transaction, identifier);
+                });
+            }
+        }
     }
 }
