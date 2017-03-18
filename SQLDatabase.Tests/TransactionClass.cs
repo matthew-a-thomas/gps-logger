1cd37784f751dcef988fa994f4d2876243986f63
Matthew Thomas <me@matthewathomas.com>
2017-03-17 21:55:56 -0400

    Added Transaction class with tests

---
+++ SQLDatabase.Tests/TransactionClass.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLDatabase.Tests
+{
+    [TestClass]
+    public class TransactionClass
+    {
+        [TestClass]
+        public class CreateCommandMethod
+        {
+            [TestMethod]
+            public void CreatesCommand()
+            {
+                using (var connection = Helper.CreateConnection())
+                using (var transaction = new Transaction(connection))
+                using (var command = transaction.CreateCommand())
+                    Assert.IsNotNull(command);
+            }
+
+            [TestMethod]
+            public void CreatesCommandThatIsInATransaction()
+            {
+                using (var connection = Helper.CreateConnection())
+                using (var transaction = new Transaction(connection))
+                using (var command = transaction.CreateCommand())
+                    Assert.IsNotNull(command?.Transaction);
+            }
+        }
+    }
+}
---
+++ SQLDatabase/Transaction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace SQLDatabase
+{
+    /// <summary>
+    /// Factory for commands that are within transactions
+    /// </summary>
+    public class Transaction : IDisposable
+    {
+        private readonly SqlTransaction _transaction;
+        private readonly SqlConnection _connection;
+
+        /// <summary>
+        /// Creates a new factory for commands that are within transactions and executed against this SqlConnection
+        /// </summary>
+        /// <param name="sqlConnection"></param>
+        public Transaction(SqlConnection sqlConnection)
+        {
+            _connection = sqlConnection;
+            _transaction = sqlConnection.BeginTransaction();
+        }
+
+        /// <summary>
+        /// Creates a new SqlCommand that is within this transaction
+        /// </summary>
+        /// <returns></returns>
+        public SqlCommand CreateCommand()
+        {
+            var command = _connection.CreateCommand();
+            command.Transaction = _transaction;
+            return command;
+        }
+
+        /// <summary>
+        /// Disposes the transaction, rolling it back
+        /// </summary>
+        public void Dispose() => _transaction.Dispose();
+    }
+}
