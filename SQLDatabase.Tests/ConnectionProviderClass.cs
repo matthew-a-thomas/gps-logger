6f71dd6cf0b1be0af8e5595d9930a79d797a4bd0
Matthew Thomas <me@matthewathomas.com>
2017-03-18 14:13:32 -0400

    Refactored to use central method

--- SQLDatabase.Tests/ConnectionProviderClass.cs
+++ SQLDatabase.Tests/ConnectionProviderClass.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SQLDatabase.Extensions;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.Threading.Tasks;
@@ -9,60 +10,70 @@ namespace SQLDatabase.Tests
     [TestClass]
     public class ConnectionProviderClass
     {
+        internal static async Task DoWithConnectionAsync(Func<SqlConnection, Task> action)
+        {
+            var provider = new ConnectionProvider(Helper.GetConnectionString());
+            using (var connection = provider.CreateConnection())
+                await action(connection);
+        }
+
         [TestClass]
         public class CreateConnectionMethod
         {
             [TestMethod]
-            public void ReturnsAnOpenConnection()
+            public async Task ReturnsAnOpenConnection()
             {
-                var provider = new ConnectionProvider(Helper.GetConnectionString());
-                using (var connection = provider.CreateConnection())
+                await DoWithConnectionAsync(async connection =>
                 {
-                    Assert.IsTrue(connection.State == ConnectionState.Open);
-                }
+                    await Task.Run(() => Assert.IsTrue(connection.State == ConnectionState.Open));
+                });
             }
 
             [TestMethod]
             public async Task ReturnsConnectionThatCanSelectFromLocations()
             {
-                var provider = new ConnectionProvider(Helper.GetConnectionString());
-                using (var connection = provider.CreateConnection())
-                using (var transaction = new Transaction(connection))
-                    await transaction.GetAsync<int>("select count(*) from locations");
+                await DoWithConnectionAsync(async connection =>
+                {
+                    using (var transaction = new Transaction(connection))
+                        await transaction.GetAsync<int>("select count(*) from locations");
+                });
             }
 
             [TestMethod]
             public async Task ReturnsConnectionThatCanSelectFromIdentifiers()
             {
-                var provider = new ConnectionProvider(Helper.GetConnectionString());
-                using (var connection = provider.CreateConnection())
-                using (var transaction = new Transaction(connection))
-                    await transaction.GetAsync<int>("select count(*) from identifiers");
+                await DoWithConnectionAsync(async connection =>
+                {
+                    using (var transaction = new Transaction(connection))
+                        await transaction.GetAsync<int>("select count(*) from identifiers");
+                });
             }
 
             [TestMethod]
             public async Task ReturnsConnectionThatCanInsertIdentifier()
             {
-                var provider = new ConnectionProvider(Helper.GetConnectionString());
-                using (var connection = provider.CreateConnection())
-                using (var transaction = new Transaction(connection)) // Is automatically rolled back
+                await DoWithConnectionAsync(async connection =>
                 {
-                    var numAffected = await transaction.ExecuteAsync("insert into identifiers(hex) values (0x0000)");
-                    Assert.AreEqual(1, numAffected);
-                }
+                    using (var transaction = new Transaction(connection)) // Is automatically rolled back
+                    {
+                        var numAffected = await transaction.ExecuteAsync("insert into identifiers(hex) values (0x0000)");
+                        Assert.AreEqual(1, numAffected);
+                    }
+                });
             }
 
             [TestMethod]
             public async Task ReturnsConnectionThatCanInsertLocation()
             {
-                var provider = new ConnectionProvider(Helper.GetConnectionString());
-                using (var connection = provider.CreateConnection())
-                using (var transaction = new Transaction(connection)) // Is automatically rolled back
+                await DoWithConnectionAsync(async connection =>
                 {
-                    var id = await transaction.GetAsync<int>("insert into identifiers(hex) values (0x0000); select cast(SCOPE_IDENTITY() as int) as id");
-                    var numAffected = await transaction.ExecuteAsync("insert into locations(id, latitude, longitude) values (@id, 0, 0)", new SqlParameter("@id", id));
-                    Assert.AreEqual(1, numAffected);
-                }
+                    using (var transaction = new Transaction(connection)) // Is automatically rolled back
+                    {
+                        var id = await transaction.GetAsync<int>("insert into identifiers(hex) values (0x0000); select cast(SCOPE_IDENTITY() as int) as id");
+                        var numAffected = await transaction.ExecuteAsync("insert into locations(id, latitude, longitude) values (@id, 0, 0)", new SqlParameter("@id", id));
+                        Assert.AreEqual(1, numAffected);
+                    }
+                });
             }
         }
     }
