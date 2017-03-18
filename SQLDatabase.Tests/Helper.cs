5d2fb01199a001802185aba06420a9a02c1b0c4d
Matthew Thomas <me@matthewathomas.com>
2017-03-17 21:37:12 -0400

    Added SQL connection helper class with tests

---
+++ SQLDatabase.Tests/Helper.cs
@@ -0,0 +1,35 @@
+﻿using Common.LocalStorage;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Text;
+
+namespace SQLDatabase.Tests
+{
+    internal static class Helper
+    {
+        /// <summary>
+        /// This method will throw exceptions until a "connection string" file is placed 
+        /// </summary>
+        /// <returns></returns>
+        private static string GetConnectionString()
+        {
+            using (var reader = File.OpenText(@"\connection string.txt")) // TODO: Change this to a location that your build server is able to access, but that is kept secret from 
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        /// <summary>
+        /// Creates a new, open connection to the SQL server
+        /// </summary>
+        /// <returns></returns>
+        public static SqlConnection CreateConnection()
+        {
+            var connection = new SqlConnection(GetConnectionString());
+            connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ SQLDatabase.Tests/HelperClass.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace SQLDatabase.Tests
+{
+    [TestClass]
+    public class HelperClass
+    {
+        [TestClass]
+        public class CreateConnectionMethod
+        {
+            [TestMethod]
+            public void ReturnsAnOpenConnection()
+            {
+                using (var connection = Helper.CreateConnection())
+                {
+                    Assert.IsTrue(connection.State == ConnectionState.Open);
+                }
+            }
+
+            [TestMethod]
+            public void ReturnsConnectionThatCanSelectFromLocations()
+            {
+                using (var connection = Helper.CreateConnection())
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "select count(*) from locations";
+                        command.ExecuteScalar();
+                    }
+                }
+            }
+
+            [TestMethod]
+            public void ReturnsConnectionThatCanSelectFromIdentifiers()
+            {
+                using (var connection = Helper.CreateConnection())
+                {
+                    using (var command = connection.CreateCommand())
+                    {
+                        command.CommandText = "select count(*) from identifiers";
+                        command.ExecuteScalar();
+                    }
+                }
+            }
+        }
+    }
+}
