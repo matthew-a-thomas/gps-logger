3856489f1d7be3989e38676896fa128d20846ff3
Matthew Thomas <me@matthewathomas.com>
2017-03-18 18:41:10 -0400

    Made internal

--- SQLDatabase/ConnectionProvider.cs
+++ SQLDatabase/ConnectionProvider.cs
@@ -8,7 +8,7 @@ namespace SQLDatabase
     /// <summary>
     /// Opens SQL connections
     /// </summary>
-    public class ConnectionProvider
+    internal class ConnectionProvider
     {
         private readonly string _connectionString;
 
