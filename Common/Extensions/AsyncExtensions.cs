719f3b46744425fda5550450339b6243063b3877
Matthew Thomas <me@matthewathomas.com>
2017-03-20 20:22:22 -0400

    New extension method for creating IObservables

---
+++ Common/Extensions/AsyncExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Extensions
+{
+    public static class AsyncExtensions
+    {
+        /// <summary>
+        /// Turns the given asynchronous functions into an IObservable
+        /// </summary>
+        public static IObservable<T> ToObservable<T>(
+            this Func<Task<T>> getAsync,
+            Func<Task<bool>> shouldLoopAsync)
+        {
+            return Observable.Create<T>(
+                observer => Task.Run(async () =>
+                {
+                    while (await shouldLoopAsync())
+                    {
+                        var value = await getAsync();
+                        observer.OnNext(value);
+                    }
+                    observer.OnCompleted();
+                })
+            );
+        }
+    }
+}
