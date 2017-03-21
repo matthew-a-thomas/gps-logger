6df27e26b319242401e53b59f40a63205adf16fe
Matthew Thomas <me@matthewathomas.com>
2017-03-20 21:28:17 -0400

    Adding MEF to SQLDatabase module

---
+++ Common/RemoteStorage/IRemoteStorageModule.cs
@@ -0,0 +1,4 @@
+﻿namespace Common.RemoteStorage
+{
+    public interface IRemoteStorageModule { }
+}
--- SQLDatabase/Module.cs
+++ SQLDatabase/Module.cs
@@ -1,14 +1,21 @@
 ﻿using Autofac;
 using SQLDatabase.RemoteStorage.Query;
 using Common.RemoteStorage.Query;
+using SQLDatabase.RemoteStorage.Command;
+using Common.RemoteStorage.Command;
+using System.Composition;
+using Common.RemoteStorage;
 
 namespace SQLDatabase
 {
-    public class Module : Autofac.Module
+    [Export(typeof(IRemoteStorageModule))]
+    public class Module : Autofac.Module, IRemoteStorageModule
     {
         protected override void Load(ContainerBuilder builder)
         {
             builder.RegisterType<LocationProvider>().As<ILocationProvider>();
+            builder.RegisterType<IdentifierPoster>().As<IIdentifierPoster>();
+            builder.RegisterType<LocationPoster>().As<ILocationPoster>();
         }
     }
 }
