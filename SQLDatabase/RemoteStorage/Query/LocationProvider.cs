45383d3054727fb6c7688a41d10a4b99b97171de
Matthew Thomas <me@matthewathomas.com>
2017-03-20 20:43:50 -0400

    Fleshed out LocationProvider

--- SQLDatabase/RemoteStorage/Query/LocationProvider.cs
+++ SQLDatabase/RemoteStorage/Query/LocationProvider.cs
@@ -4,15 +4,47 @@ using System.Collections.Generic;
 using System.Text;
 using Common.RemoteStorage.Models;
 using System.Threading.Tasks;
+using SQLDatabase.Extensions;
+using System.Data.SqlClient;
 
 namespace SQLDatabase.RemoteStorage.Query
 {
     internal class LocationProvider : ILocationProvider
     {
+        private readonly Transaction _transaction;
+
+        public LocationProvider(Transaction transaction)
+        {
+            _transaction = transaction;
+        }
+
         public async Task<IEnumerable<IdentifiedLocation>> GetAllLocationsAsync(byte[] forIdentifier)
         {
-            await Task.Run(() => { });
-            throw new NotImplementedException();
+            var result = new LinkedList<IdentifiedLocation>();
+            await _transaction.ProcessResultsAsync(@"
+select
+    locations.*
+from
+    identifiers
+    join locations on
+        identifiers.id = locations.id
+where
+    identifiers.hex = @hex
+",
+                record =>
+                {
+                    var location = new IdentifiedLocation
+                    {
+                        Identifier = forIdentifier,
+                        Latitude = (double)record["latitude"],
+                        Longitude = (double)record["longitude"],
+                        Timestamp = (DateTime)record["timestamp"]
+                    };
+                    result.AddLast(location);
+                },
+                new SqlParameter("@hex", forIdentifier)
+            );
+            return result;
         }
     }
 }
