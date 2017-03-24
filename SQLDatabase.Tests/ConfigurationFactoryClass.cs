b69560b9b0c1c9b88e354bb7dad47f999a56eacc
Matthew Thomas <me@matthewathomas.com>
2017-03-24 14:45:54 -0400

    Additional test logic

--- SQLDatabase.Tests/ConfigurationFactoryClass.cs
+++ SQLDatabase.Tests/ConfigurationFactoryClass.cs
@@ -20,16 +20,19 @@ namespace SQLDatabase.Tests
             }
 
             [TestMethod]
-            public void ReturnsSomethingWithValues()
+            public void ReturnsValuesFromSources()
             {
-                var factory = new ConfigurationFactory(new IConfigurationSource[] { new MemoryConfigurationSource { InitialData = new[]
+                var data = new[]
                 {
-                    new KeyValuePair<string, string>("hello", "world")
-                } } });
+                    new KeyValuePair<string, string>("hello", "world"),
+                    new KeyValuePair<string, string>("goodbye", "moon")
+                };
+                var factory = new ConfigurationFactory(new IConfigurationSource[] { new MemoryConfigurationSource { InitialData = data} });
                 var config = factory.CreateConfiguration();
                 var children = config.GetChildren();
-                Assert.IsNotNull(children);
-                Assert.IsTrue(children.Any());
+                Assert.IsNotNull(children, $"{nameof(children)} is null");
+                Assert.IsTrue(children.Any(), $"{nameof(children)} is empty");
+                Assert.IsTrue(config.AsEnumerable().Zip(data, (one, other) => one.Key == other.Key && one.Value == other.Value).Aggregate(true, (a, b) => a && b), $"{nameof(config)} doesn't return name values we put in");
             }
         }
     }
