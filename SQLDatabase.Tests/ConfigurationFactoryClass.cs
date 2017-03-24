54a237d0abee620ee1823521985b3377521a5c85
Matthew Thomas <me@matthewathomas.com>
2017-03-24 00:06:19 -0400

    Fixed bug with loading configuration

---
+++ SQLDatabase.Tests/ConfigurationFactoryClass.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SQLDatabase.Tests
+{
+    [TestClass]
+    public class ConfigurationFactoryClass
+    {
+        [TestClass]
+        public class CreateConfigurationMethod
+        {
+            [TestMethod]
+            public void IsErrorFree()
+            {
+                var factory = new ConfigurationFactory();
+                factory.CreateConfiguration();
+            }
+
+            [TestMethod]
+            public void ReturnsSomethingWithValues()
+            {
+                var factory = new ConfigurationFactory();
+                var config = factory.CreateConfiguration();
+                var children = config.GetChildren();
+                Assert.IsNotNull(children);
+                Assert.IsTrue(children.Any());
+            }
+        }
+    }
+}
--- SQLDatabase/ConfigurationFactory.cs
+++ SQLDatabase/ConfigurationFactory.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.EnvironmentVariables;
 using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.FileProviders;
 
 namespace SQLDatabase
 {
@@ -11,7 +12,7 @@ namespace SQLDatabase
         public IConfiguration CreateConfiguration()
         {
             var config = new ConfigurationBuilder()
-                .Add(new JsonConfigurationSource { Path = @"\sql.json", Optional = true })
+                .Add(new JsonConfigurationSource { Path = "sql.json", Optional = true, FileProvider = new PhysicalFileProvider(@"C:\") })
                 .Add(new EnvironmentVariablesConfigurationSource { Prefix = "SQL_" })
                 .Build();
             return config;
