d2c2ea81dd7344849703fcc1e90e2e9934d24fd1
Matthew Thomas <me@matthewathomas.com>
2017-03-24 14:46:20 -0400

    Initial unit test class

---
+++ SQLDatabase.Tests/RemoteStorage/Query/LocationProviderClass.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SQLDatabase.Tests.RemoteStorage.Query
+{
+    [TestClass]
+    public class LocationProviderClass
+    {
+        [TestClass]
+        public class GetAllLocationsAsyncMethod
+        {
+            [TestMethod]
+            public void NotImplemented()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
