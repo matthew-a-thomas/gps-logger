de546ff40c571869c514d6a915138c67315b2097
Matthew Thomas <me@matthewathomas.com>
2017-03-25 12:03:45 -0400

    ADd'l tests

---
+++ SQLDatabase.Tests/TransactionClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Common.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace SQLDatabase.Tests
+{
+    [TestClass]
+    public class TransactionClass
+    {
+        [TestClass]
+        public class Constructor
+        {
+            [TestMethod]
+            [SuppressMessage("ReSharper", "ObjectCreationAsStatement")]
+            public void HandlesNullParameters()
+            {
+                using (new Transaction(null, null)) { }
+                using (new Transaction(null, new ConnectionOptions())) { }
+                using (new Transaction(new Factory<ConnectionOptions, SqlConnection>(_ => null), null)) { }
+                using (new Transaction(new Factory<ConnectionOptions, SqlConnection>(_ => null), new ConnectionOptions())) { }
+            }
+
+            [TestMethod]
+            public void HandlesNopInterfaces()
+            {
+                // ReSharper disable once ObjectCreationAsStatement
+                using (new Transaction(
+                    new Mock<IFactory<ConnectionOptions, SqlConnection>>().Object,
+                    new ConnectionOptions())) { }
+            }
+        }
+    }
+}
