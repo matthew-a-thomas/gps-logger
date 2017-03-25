47c7f4fc0103e5de4fb80200e453b6f97e93e218
Matthew Thomas <me@matthewathomas.com>
2017-03-25 11:45:54 -0400

    Add'l tests

---
+++ Common.Tests/Extensions/IFactoryExtensionsClass.cs
@@ -0,0 +1,34 @@
+﻿using Common.Extensions;
+using Common.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace Common.Tests.Extensions
+{
+    [TestClass]
+    public class IFactoryExtensionsClass
+    {
+        [TestClass]
+        public class ChainIntoMethod
+        {
+            [TestMethod]
+            public void HandlesNopInterfaces()
+            {
+                // ReSharper disable once InvokeAsExtensionMethod
+                IFactoryExtensions.ChainInto<object, object, object>(
+                    new Mock<IFactory<object, object>>().Object,
+                    new Mock<IFactory<object, object>>().Object
+                );
+            }
+
+            [TestMethod]
+            public void HandlesNullParameters()
+            {
+                IFactoryExtensions.ChainInto<object, object, object>(null, null);
+                // ReSharper disable once InvokeAsExtensionMethod
+                IFactoryExtensions.ChainInto<object, object, object>(new Factory<object, object>(_ => null), null);
+                IFactoryExtensions.ChainInto<object, object, object>(null, new Factory<object, object>(_ => null));
+            }
+        }
+    }
+}
