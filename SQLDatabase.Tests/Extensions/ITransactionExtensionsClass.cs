ef8391763589bad838aa7c65a4e8d8f064e1390c
Matthew Thomas <me@matthewathomas.com>
2017-03-25 11:13:47 -0400

    Additional tests

---
+++ SQLDatabase.Tests/Extensions/ITransactionExtensionsClass.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SQLDatabase.Extensions;
+
+namespace SQLDatabase.Tests.Extensions
+{
+    [TestClass]
+    public class ITransactionExtensionsClass
+    {
+        [TestClass]
+        public class GetScalarAsyncMethod
+        {
+            [TestMethod]
+            public async Task HandlesNopInterfaces()
+            {
+                var mockedTransaction = new Mock<ITransaction>();
+                // ReSharper disable once InvokeAsExtensionMethod
+                await ITransactionExtensions.GetScalarAsync<object>(mockedTransaction.Object, Commands.Command.Create(""));
+            }
+
+            [TestMethod]
+            public async Task HandlesNullParameters()
+            {
+                await ITransactionExtensions.GetScalarAsync<object>(null, null);
+            }
+        }
+    }
+}
