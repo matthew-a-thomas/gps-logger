b99fd87c8361035827d2fc519142d989533c1238
Matthew Thomas <me@matthewathomas.com>
2017-03-25 12:01:23 -0400

    New extension method with tests

---
+++ Common/Extensions/ActionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace Common.Extensions
+{
+    public static class ActionExtensions
+    {
+        /// <summary>
+        /// Creates a new action that will invoke the given action only up to once
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static Action MakeSingular(this Action action)
+        {
+            if (action == null)
+                return null;
+            var numInvocations = 0;
+            return () =>
+            {
+                if (Interlocked.Increment(ref numInvocations) != 1) return;
+                action();
+            };
+        }
+    }
+}
