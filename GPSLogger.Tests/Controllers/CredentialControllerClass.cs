3db015701017f76d5baa189c0057055d42061875
Matthew Thomas <me@matthewathomas.com>
2017-03-26 15:57:44 -0400

    Add'l tests

---
+++ GPSLogger.Tests/Controllers/CredentialControllerClass.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common.Messages;
+using Common.Security;
+using GPSLogger.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GPSLogger.Tests.Controllers
+{
+    [TestClass]
+    public class CredentialControllerClass
+    {
+        private static void TestWithNullAndNopConstructorParameters(Action<CredentialController> test)
+        {
+            test(new CredentialController(
+                null,
+                null,
+                null));
+            test(new CredentialController(
+                null,
+                null,
+                new Mock<IMessageHandler<bool, Credential<string>>>().Object));
+            test(new CredentialController(
+                null,
+                id => new ValueTask<Credential<byte[]>>(new Credential<byte[]> { ID = new byte[0], Secret = new byte[0] }),
+                null));
+            test(new CredentialController(
+                null,
+                id => new ValueTask<Credential<byte[]>>(new Credential<byte[]> { ID = new byte[0], Secret = new byte[0] }),
+                new Mock<IMessageHandler<bool, Credential<string>>>().Object));
+            test(new CredentialController(
+                () => new ValueTask<byte[]>(new byte[0]),
+                null,
+                null));
+            test(new CredentialController(
+                () => new ValueTask<byte[]>(new byte[0]),
+                null,
+                new Mock<IMessageHandler<bool, Credential<string>>>().Object));
+            test(new CredentialController(
+                () => new ValueTask<byte[]>(new byte[0]),
+                id => new ValueTask<Credential<byte[]>>(new Credential<byte[]> { ID = new byte[0], Secret = new byte[0] }),
+                null));
+            test(new CredentialController(
+                () => new ValueTask<byte[]>(new byte[0]),
+                id => new ValueTask<Credential<byte[]>>(new Credential<byte[]> { ID = new byte[0], Secret = new byte[0] }),
+                new Mock<IMessageHandler<bool, Credential<string>>>().Object));
+        }
+
+        [TestClass]
+        public class Constructor
+        {
+            [TestMethod]
+            public void WorksWithNullAndNopParameters()
+            {
+                TestWithNullAndNopConstructorParameters(_ => { });
+            }
+        }
+    }
+}
