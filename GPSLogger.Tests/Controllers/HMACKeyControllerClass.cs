96dc64725726d2082793329c235e738afe17f721
Matthew Thomas <me@matthewathomas.com>
2017-03-26 22:27:53 -0400

    Add'l tests

--- GPSLogger.Tests/Controllers/HMACKeyControllerClass.cs
+++ GPSLogger.Tests/Controllers/HMACKeyControllerClass.cs
@@ -40,10 +40,93 @@ namespace GPSLogger.Tests.Controllers
                 var controller = new HMACKeyController(store);
                 await controller.PostAsync(new HMACKeyController.PostParameters
                 {
-                    NewKey = (new byte[100]).ToHexString()
+                    NewKey = new byte[100].ToHexString()
                 });
                 var get = await controller.GetAsync();
                 Assert.IsTrue(get);
+            }
+        }
+
+        [TestClass]
+        public class PostAsyncClass
+        {
+            [TestMethod]
+            public async Task HandlesNopConstructorParameter()
+            {
+                var mockedStore = new Mock<IStorage>();
+                var controller = new HMACKeyController(mockedStore.Object);
+                await controller.PostAsync(new HMACKeyController.PostParameters { NewKey = new byte[100].ToHexString() });
+            }
+
+            [TestMethod]
+            public async Task CannotPostEmptyKey()
+            {
+                var controller = new HMACKeyController(CreateStore());
+                bool failed;
+                try
+                {
+                    await controller.PostAsync(new HMACKeyController.PostParameters());
+                    failed = false;
+                }
+                catch
+                {
+                    failed = true;
+                }
+                Assert.IsTrue(failed);
+            }
+
+            [TestMethod]
+            public async Task CannotPostNull()
+            {
+                var controller = new HMACKeyController(CreateStore());
+                bool failed;
+                try
+                {
+                    await controller.PostAsync(null);
+                    failed = false;
+                }
+                catch
+                {
+                    failed = true;
+                }
+                Assert.IsTrue(failed);
+            }
+
+            [TestMethod]
+            public async Task CannotPostTwice()
+            {
+                var controller = new HMACKeyController(CreateStore());
+                await controller.PostAsync(new HMACKeyController.PostParameters { NewKey = new byte[100].ToHexString() });
+                bool failed;
+                try
+                {
+                    await controller.PostAsync(
+                        new HMACKeyController.PostParameters {NewKey = new byte[100].ToHexString()});
+                    failed = false;
+                }
+                catch
+                {
+                    failed = true;
+                }
+                Assert.IsTrue(failed);
+            }
+
+            [TestMethod]
+            public async Task CannotPostSmallKey()
+            {
+                var controller = new HMACKeyController(CreateStore());
+                bool failed;
+                try
+                {
+                    await controller.PostAsync(
+                        new HMACKeyController.PostParameters { NewKey = new byte[1].ToHexString() });
+                    failed = false;
+                }
+                catch
+                {
+                    failed = true;
+                }
+                Assert.IsTrue(failed);
             }
         }
     }
