c6e669a238a08db662bdb6c0ad458858c4853493
Matthew Thomas <me@matthewathomas.com>
2017-03-26 22:30:25 -0400

    Stubbed tests

---
+++ GPSLogger.Tests/Controllers/LocationControllerClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GPSLogger.Tests.Controllers
+{
+    [TestClass]
+    public class LocationControllerClass
+    {
+        [TestClass]
+        public class GetAsyncMethod
+        {
+            
+        }
+
+        [TestClass]
+        public class PostAsyncMethod
+        {
+            
+        }
+    }
+}
