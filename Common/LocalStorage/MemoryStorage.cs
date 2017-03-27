5f12ed310e7c3c56b499079fa84eaed33eb2fdeb
Matthew Thomas <me@matthewathomas.com>
2017-03-26 22:19:52 -0400

    Added in-memory implementation of storage

---
+++ Common/LocalStorage/MemoryStorage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Concurrent;
+using System.Threading.Tasks;
+
+namespace Common.LocalStorage
+{
+    /// <summary>
+    /// In-memory implementation of storage
+    /// </summary>
+    public class MemoryStorage : IStorage
+    {
+        private ConcurrentDictionary<string, byte[]> _memory = new ConcurrentDictionary<string, byte[]>();
+
+        public ValueTask<bool> ExistsAsync(string key) => new ValueTask<bool>(_memory.ContainsKey(key));
+
+        public ValueTask<byte[]> GetAsync(string key) => _memory.TryGetValue(key, out byte[] value) ? new ValueTask<byte[]>(value) : new ValueTask<byte[]>((byte[])null);
+
+        public Task SetAsync(string key, byte[] contents)
+        {
+            _memory.AddOrUpdate(key, _ => contents,
+                (_, __) => contents);
+            return Task.CompletedTask;
+        }
+    }
+}
