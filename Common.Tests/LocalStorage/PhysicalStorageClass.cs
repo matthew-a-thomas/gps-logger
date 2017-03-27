c956cff73e25c25afbf4ae72f886ee21230d81d3
Matthew Thomas <me@matthewathomas.com>
2017-03-27 10:18:48 -0400

    Made generic

--- Common.Tests/LocalStorage/PhysicalStorageClass.cs
+++ Common.Tests/LocalStorage/PhysicalStorageClass.cs
@@ -21,7 +21,7 @@ namespace Common.Tests.LocalStorage
             return directoryInfo;
         }
 
-        internal static async Task DoWithTempPersistentStoreAsync(Func<PhysicalStorage, Task> actionAsync)
+        internal static async Task DoWithTempPersistentStoreAsync(Func<IStorage, Task> actionAsync)
         {
             var directory = CreateTempDirectory();
             var store = new PhysicalStorage(directory, 100);
