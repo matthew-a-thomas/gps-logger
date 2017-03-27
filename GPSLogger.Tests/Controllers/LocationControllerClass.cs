f2cb023162b103732627e26798fa2276d54e433f
Matthew Thomas <me@matthewathomas.com>
2017-03-26 22:35:49 -0400

    Add'l test

--- GPSLogger.Tests/Controllers/LocationControllerClass.cs
+++ GPSLogger.Tests/Controllers/LocationControllerClass.cs
@@ -1,7 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Messages;
+using GPSLogger.Controllers;
+using GPSLogger.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 
 namespace GPSLogger.Tests.Controllers
 {
@@ -11,7 +15,20 @@ namespace GPSLogger.Tests.Controllers
         [TestClass]
         public class GetAsyncMethod
         {
-            
+            [TestMethod]
+            public async Task ReturnsLocations()
+            {
+                var controller = new LocationController(
+                    id => new ValueTask<IEnumerable<Location>>(new[]
+                        {new Location {Latitude = 0, Longitude = 1}}),
+                    (id, location) => Task.CompletedTask,
+                    new Mock<IMessageHandler<Location, bool>>().Object
+                );
+                var locations = await controller.GetAsync("00");
+                Assert.IsNotNull(locations);
+                Assert.IsTrue(locations.Any());
+                Assert.IsNotNull(locations.First());
+            }
         }
 
         [TestClass]
