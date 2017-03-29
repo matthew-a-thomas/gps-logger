73112aa4adb21537a9e1ff0951a13c9f6776e753
Matthew Thomas <me@matthewathomas.com>
2017-03-29 10:31:11 -0400

    Fixed runtime issue with first arg's format

--- Common/Utilities/CommandLineArgumentsProvider.cs
+++ Common/Utilities/CommandLineArgumentsProvider.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 namespace Common.Utilities
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class CommandLineArgumentsProvider : IArgumentsProvider
     {
-        public string[] GetArguments() => Environment.GetCommandLineArgs();
+        public string[] GetArguments() => Environment.GetCommandLineArgs().Skip(1).ToArray();
     }
 }
