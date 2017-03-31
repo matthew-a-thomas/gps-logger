f7fecb1444149fec743c831660c33dfa0888dda2
Matthew Thomas <me@matthewathomas.com>
2017-03-31 15:36:15 -0400

    Fixed issue with parameters not mapping

--- GPSLogger/Controllers/TimeController.cs
+++ GPSLogger/Controllers/TimeController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Threading.Tasks;
 using Common.Messages;
 using Common.Security.Signing;
@@ -9,6 +10,17 @@ namespace GPSLogger.Controllers
     [Route("api/[controller]")]
     public class TimeController : ControllerBase
     {
+        // ReSharper disable once ClassNeverInstantiated.Global
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public class GetParameters
+        {
+            public string HMAC { get; set; }
+            public string ID { get; set; }
+            public string Salt { get; set; }
+            public long UnixTime { get; set; }
+            public bool Contents { get; set; }
+        }
+
         private readonly IMessageHandler<bool, long> _messageHandler;
         
         public TimeController(
@@ -22,6 +34,23 @@ namespace GPSLogger.Controllers
         /// </summary>
         /// <returns></returns>
         [HttpGet]
-        public async Task<SignedMessage<long>> GetAsync(SignedMessage<bool> request) => await _messageHandler.CreateResponseAsync(request, valid => Task.FromResult(DateTimeOffset.Now.ToUnixTimeSeconds()));
+        public async Task<SignedMessage<long>> GetAsync(GetParameters parameters)
+        {
+            var signedRequest = new SignedMessage<bool>
+            {
+                HMAC = parameters.HMAC,
+                Message = new Message<bool>
+                {
+                    Contents = parameters.Contents,
+                    ID = parameters.ID,
+                    Salt = parameters.Salt,
+                    UnixTime = parameters.UnixTime
+                }
+            };
+            return await _messageHandler.CreateResponseAsync(
+                signedRequest,
+                valid => Task.FromResult(DateTimeOffset.Now.ToUnixTimeSeconds())
+                );
+        }
     }
 }
