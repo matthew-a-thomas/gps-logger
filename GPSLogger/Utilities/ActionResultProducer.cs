5cfda2d71ea57eaf3b6a19e30a929c6466c15b7b
Matthew Thomas <me@matthewathomas.com>
2017-04-03 12:32:46 -0400

    Added an IActionResult producer

---
+++ GPSLogger/Utilities/ActionResultProducer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common.Errors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GPSLogger.Utilities
+{
+    public class ActionResultProducer : IActionResultProducer
+    {
+        private readonly IErrorHandler _errorHandler;
+
+        public ActionResultProducer(IErrorHandler errorHandler)
+        {
+            _errorHandler = errorHandler;
+        }
+
+        public async Task<IActionResult> ProduceAsync(Func<Task> fromAsync)
+        {
+            try
+            {
+                await fromAsync();
+                return new OkResult();
+            }
+            catch (Exception e)
+            {
+                var response = _errorHandler.Handle(e);
+                return new ObjectResult(response)
+                {
+                    StatusCode = 500
+                };
+            }
+        }
+
+        public async Task<IActionResult> ProduceAsync<T>(Func<Task<T>> fromAsync)
+        {
+            try
+            {
+                var response = await fromAsync();
+                return new OkObjectResult(response);
+            }
+            catch (Exception e)
+            {
+                var response = _errorHandler.Handle(e);
+                return new ObjectResult(response)
+                {
+                    StatusCode = 500
+                };
+            }
+        }
+    }
+}
---
+++ GPSLogger/Utilities/IActionResultProducer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GPSLogger.Utilities
+{
+    public interface IActionResultProducer
+    {
+        Task<IActionResult> ProduceAsync(Func<Task> fromAsync);
+        Task<IActionResult> ProduceAsync<T>(Func<Task<T>> fromAsync);
+    }
+}
