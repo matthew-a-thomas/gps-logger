0d1a53a6f0312491c99639a1d9f67234be3dce2d
Matthew Thomas <me@matthewathomas.com>
2017-04-04 15:29:24 -0400

    Spurious comment

--- Common/Utilities/AutoFinalizer.cs
+++ Common/Utilities/AutoFinalizer.cs
@@ -6,12 +6,6 @@ namespace Common.Utilities
     /// <summary>
     /// Owns an action and keeps it alive outside the garbage collector until finalized
     /// </summary>
-    /// <remarks>
-    /// Just keep a reference to this in your class
-    /// When you're GC'd this should be marked for finalize.
-    /// Upon finalization, this will invoke the method that's been kept alive.
-    /// If then there are no remaining references to the object, it will be GC'd on a later cycle
-    /// </remarks>
     public class AutoFinalizer
     {
         private GCHandle _handle;
