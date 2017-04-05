4e3e13f5a3d72247f8b78863183f78ddeb0d527c
Matthew Thomas <me@matthewathomas.com>
2017-04-05 17:01:12 -0400

    Fleshing out the JS library a little more

--- GPSLogger/wwwroot/js/js.js
+++ GPSLogger/wwwroot/js/js.js
@@ -2,11 +2,15 @@
 (function () {
     if (!jQuery)
         throw "Need jQuery";
+
+    
+
     Logger = function (options) {
+        var _this = this;
+
         this.server = options.server;
         this.credential = { id: null, secret: null };
 
-        var _this = this;
         var ajax = function (url, callback, data, type) {
             var options = {
                 url: _this.server + url,
@@ -16,11 +20,11 @@
             };
             jQuery.ajax(options);
         };
-        var post = function (url, callback, data) {
-            ajax(url, callback, data, "POST");
-        };
         var get = function (url, callback, data) {
             ajax(url, callback, data, "GET");
+        };
+        var post = function (url, callback, data) {
+            ajax(url, callback, data, "POST");
         };
 
         this.getCredential = function (callback) {
@@ -29,5 +33,9 @@
                 callback(_this.credential);
             });
         }
+
+        this.postLocation = function (location, callback) {
+
+        };
     };
 })();
