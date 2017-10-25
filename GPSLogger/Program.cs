43bbe4e769f4301ee945862f9519037177372b47
Matt Thomas <me@matthewathomas.com>
2017-10-24 23:47:40 -0400

    Added Microsoft.AspNetCore.Mvc.Razor.Host package and made it a compile-time dependency of GPSLogger

--- GPSLogger/Program.cs
+++ GPSLogger/Program.cs
@@ -3,12 +3,15 @@ using Microsoft.AspNetCore.Hosting;
 
 namespace GPSLogger
 {
+    using System;
     using Microsoft.AspNetCore;
 
     public class Program
     {
         public static void Main(string[] args)
         {
+            GC.KeepAlive(typeof(Microsoft.AspNetCore.Mvc.Razor.Host.ViewComponentTagHelperDescriptorConventions));
+            
             var host =
                 WebHost
                     .CreateDefaultBuilder()
