1810fc88e9c595b4e7c9ea32daeb42513269fe9a
Matt Thomas <me@matthewathomas.com>
2017-10-24 23:53:55 -0400

    Removed Microsoft.AspNetCore.Mvc.Razor.Host package

--- GPSLogger/Program.cs
+++ GPSLogger/Program.cs
@@ -1,17 +1,13 @@
-﻿using System.IO;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 
 namespace GPSLogger
 {
-    using System;
     using Microsoft.AspNetCore;
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            GC.KeepAlive(typeof(Microsoft.AspNetCore.Mvc.Razor.Host.ViewComponentTagHelperDescriptorConventions));
-            
             var host =
                 WebHost
                     .CreateDefaultBuilder()
